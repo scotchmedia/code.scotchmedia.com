dd6e2373305415d4ee44eb1d9ecf7a18982b4df3
Kyle Finley <kyle.finley@gmail.com>
2012-11-29 17:48:37 -0600

    changed app name

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from webapp2_extras.routes import RedirectRoute
 
 
 routes = [
-    RedirectRoute('/', redirect_to='/engineauth/index.html'),
-    RedirectRoute('/engineauth', redirect_to='/engineauth/index.html'),
+    RedirectRoute('/', redirect_to='/engineauth/docs/index.html'),
+    RedirectRoute('/engineauth', redirect_to='/engineauth/docs/index.html'),
+    RedirectRoute('/engineauth/', redirect_to='/engineauth/docs/index.html'),
+    RedirectRoute('/engineauth/docs', redirect_to='/engineauth/docs/index.html'),
+    RedirectRoute('/engineauth/docs/', redirect_to='/engineauth/docs/index.html'),
 ]
 
 application = webapp2.WSGIApplication(routes)
